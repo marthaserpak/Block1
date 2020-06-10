578963d95350e950019839af39c823a00c22629c
marthaserpak <m.v.serpak@gmail.com>
2020-06-10 14:41:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/block1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.block1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements
+View.OnClickListener {
+
+    Button buttonBlue, buttonPink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonBlue = findViewById(R.id.btn_blueInvisible);
+        buttonBlue.setOnClickListener(this);
+
+        buttonPink = new Button(getApplicationContext());
+        buttonPink.setTextColor(Color.BLACK);
+        buttonPink.setBackgroundColor(Color.parseColor("#FD9BF3"));
+        buttonPink.setText("The Pink Panther's To Do List");
+
+        RelativeLayout relativeLayout = findViewById(R.id.relativeLayout_mainScreen);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.BELOW, buttonBlue.getId());
+        relativeLayout.addView(buttonPink, params);
+        buttonPink.setOnClickListener(this);
+    }
+
+    public void toDo(View v) {
+        if (v.equals(buttonBlue)) {
+            v.setVisibility(View.INVISIBLE);
+        }
+
+        if (v.equals(buttonPink)) {
+            Toast.makeText(getApplicationContext(),
+                    "to do to do to do...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        toDo(v);
+    }
+}
